566981caade982c63f949ed738222a594a7bfc0a
Aparna Karanth <aparnakaranth654@gmail.com>
2025-09-24 16:48:56 +0530

    Update RR.c

--- lab6/RR.c
+++ lab6/RR.c
@@ -1,49 +1,62 @@
 #include <stdio.h>
+//round robin
 struct P{
-	int bt,wt,tat,rt,pid
+	int id,at,bt,wt,tat,rt;
 };
 int main(){
 	int n,tq;
-	int time=0, done=0;
-	printf("Enter number of processes: \n");
+	printf("ENter n: ");
 	scanf("%d", &n);
 	struct P p[n];
 	for(int i=0;i<n;i++){
-		p[i].pid=i+1;
-		printf("enter burst time: ");
-		scanf("%d",&p[i].bt);
+		p[i].id=i+1;
+		printf("ENter at: ");
+		scanf("%d", &p[i].at);
+		printf("ENter bt: ");
+		scanf("%d", &p[i].bt);
 		p[i].rt=p[i].bt;
-		p[i].wt=p[i].tat=0;
+		p[i].wt=0;
+		p[i].tat=0;
 	}
-	printf("enter tq: ");
-	scanf("%d",&tq);
+	printf("ENter tq: ");
+	scanf("%d", &tq);
+	int time=0, done=0;
+	int gnattpid[100], gnatts[100],gnatte[100], gidx=0;
 	while(done<n){
+		int executed=0;
 		for(int i=0;i<n;i++){
-			if(p[i].rt>0){
+			if(p[i].rt>0 && p[i].at<=time){
+				executed=1;
+				int start=time;
 				if(p[i].rt>tq){
 					time+=tq;
 					p[i].rt-=tq;
 				}else{
 					time+=p[i].rt;
-					p[i].wt=time-p[i].bt;
+					p[i].wt=time-p[i].bt-p[i].at;
 					p[i].rt=0;
 					done++;
 				}
+				gnattpid[gidx]=p[i].id;
+				gnatts[gidx]=start;
+				gnatte[gidx]=time;
+				gidx++;
 			}
 		}
+		if(!executed) time++;
 	}
 	for(int i=0;i<n;i++){
 		p[i].tat=p[i].bt+p[i].wt;
-		printf("P%d\t%d\t%d\t%d\n",p[i].pid,p[i].bt,p[i].wt, p[i].tat);
-	}
-	float totwt=0, totat=0;
-	for(int i=0;i<n;i++){
-		totwt+=p[i].wt;
-		totat+=p[i].tat;
+		printf("P%d\t%d\t%d\t%d\t%d\n",p[i].id, p[i].at, p[i].bt,p[i].wt,p[i].tat);
 		
 	}
-	printf("AVGV TOT WT IS: %f", totwt/n);
-	printf("AVGV TOT TAT IS: %f", totat/n);
+	for(int i=0;i<gidx;i++){
+		printf("P%d: %d-%d", gnattpid[i], gnatts[i], gnatte[i]);
+		if(i!=gidx-1) printf(", ");
+	}printf("\n");
 	
 	return 0;
+	
+	
+	
 }
